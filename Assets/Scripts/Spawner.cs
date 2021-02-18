0e9ab7ae95ae59e54b343b872bcdb18d6467c233
anirml <cph-jj443@cphbusiness.dk>
2021-02-18 16:15:52 +0100

    Distance Script, Spawner Script, Metaldetektor sounds

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject Cube1prefab;
+
+
+    public Vector3 center;
+    public Vector3 size;
+
+    private int sumAmount = 1;
+
+    void Start()
+    {
+
+        Cube1prefab = GameObject.FindWithTag("Cube1");
+
+        for (int i = 0; i < sumAmount; i++)
+        {
+            SpawnItems();
+        }
+
+    }
+
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = new Color(1, 0, 0, 0.5f);
+        Gizmos.DrawCube(center, size);
+    }
+
+
+    public void SpawnItems()
+    {
+        Destroy(Cube1prefab);
+
+        Vector3 pos = center + new Vector3(Random.Range(-size.x/2 , size.x/2), Random.Range(-size.y / 2, size.y / 2), Random.Range(-size.z / 2, size.z / 2));
+
+            Instantiate(Cube1prefab, pos, Quaternion.identity);
+        
+    }
+
+
+
+}
--- Assets/Scripts/distance.cs
+++ Assets/Scripts/distance.cs
@@ -2,24 +2,124 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class distance : MonoBehaviour
+[RequireComponent(typeof(AudioSource))]
+public class Distance : MonoBehaviour
 {
-    
-        public GameObject Cube1;
-        public GameObject Cube2;
 
-  /*  void Start()
+    public GameObject shadow;
+    public GameObject Cube1;
+
+    public AudioSource m_bib_sound;
+
+    public float volume = 1.0f;
+    public float timeBetweenShots = 2.0f;
+    float timer = 0.0f;
+
+    void Start()
     {
 
-        Cube1 = GameObject.FindWithTag("Cube1").GetComponent<GameObject>();
-        Cube2 = GameObject.FindWithTag("Cube2");
+        shadow = GameObject.FindWithTag("Player");
+        Cube1 = GameObject.FindWithTag("Cube1");
+ 
+        m_bib_sound = GetComponent<AudioSource>();
+        m_bib_sound.clip = Resources.Load<AudioClip>("Sounds/metal_detector_bib_sound");
 
-    } */
-    // Update is called once per frame
-    void Update()
-    {
-        float distance = Vector3.Distance(Cube1.transform.position, Cube2.transform.position);
-
-        Debug.Log(distance);
     }
+
+
+
+    public void playSound()
+    {
+        m_bib_sound.PlayOneShot(m_bib_sound.clip,volume);
+    }
+
+
+
+    void FixedUpdate()
+    {
+        //we destroyed the old cube so need to search for the new copy
+        Cube1 = GameObject.Find("Cube1(Clone)");
+
+        //TODO spawn med forskelligt navn, brug navnene til at finde nærmeste object.
+
+        float Distance = Vector3.Distance(shadow.transform.position,Cube1.transform.position);
+
+        Debug.Log(Distance);
+
+
+
+        //Lav dette til switch Case
+        if (Distance > 10)
+        {
+            timer += 0.008f;
+            if (timer > timeBetweenShots)
+            {
+                playSound();
+                timer = 0;
+            }
+        }
+
+        if (Distance < 10 && Distance > 8)
+        {
+            timer += 0.012f;
+            if (timer > timeBetweenShots)
+            {
+                playSound();
+                timer = 0;
+            }
+        }
+
+        if (Distance < 8 && Distance > 6)
+        {
+            timer += 0.020f;
+            if (timer > timeBetweenShots)
+            {
+                playSound();
+                timer = 0;
+            }
+        }
+
+        if (Distance < 6 && Distance > 4)
+        {
+            timer += 0.030f;
+            if (timer > timeBetweenShots)
+            {
+                playSound();
+                timer = 0;
+            }
+        }
+
+        if (Distance < 4 && Distance > 2)
+        {
+            timer += 0.050f;
+            if (timer > timeBetweenShots)
+            {
+                playSound();
+                timer = 0;
+            }
+        }
+
+        if (Distance < 2 && Distance > 1)
+        {
+            timer += 0.080f;
+            if (timer > timeBetweenShots)
+            {
+                playSound();
+                timer = 0;
+            }
+        }
+
+        if (Distance < 1)
+        {
+            timer += 0.2f;
+            if (timer > timeBetweenShots)
+            {
+                playSound();
+                timer = 0;
+            }
+        }
+
+    }
+
+
 }
